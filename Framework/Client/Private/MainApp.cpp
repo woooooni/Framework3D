6203ef03ed4a7e402f1c28b96012fce51228a1c8
Tiny-Prince <rlaxodnjs6574@gmail.com>
2023-08-18 20:42:46 +0900

    First Commit

---
+++ Framework/Client/Private/MainApp.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include "..\Public\MainApp.h"
+
+
+CMainApp::CMainApp()
+{
+}
+
+
+CMainApp::~CMainApp()
+{
+}
+
+HRESULT CMainApp::Initialize()
+{
+	/* 1. 내 게임의 초기화를 수행할꺼야. */
+	/* 1-1. 그래픽장치를 초기화한다. */
+	/* 1-2. 사운드장치를 초기화한다. */
+	/* 1-3. 입력장치를 초기화한다. */
+	/* 1-4. 게임내에서 사용할 레벨(씬)을 생성한다.   */
+	/* 1-4-1. 게임내에서 사용할 여러 자원(텍스쳐, 모델, 객체) 들을 준비한다.  */
+
+	return S_OK;
+}
+
+void CMainApp::Tick()
+{
+	/* 게임내에 존재하는 여러 객체들의 갱신. */
+	/* 레벨의 갱신 */
+}
+
+HRESULT CMainApp::Render()
+{
+	/* 게임내에 존재하는 여러 객체들의 렌더링. */
+	return S_OK;
+}
---
+++ Framework/Client/Public/Client_Defines.h
@@ -0,0 +1,6 @@
+#pragma once
+
+/* 클라이언트에 존재하는 모든 클래스가 인클루드해야할 내용을 담는다. */
+#include "d3d11.h"
+
+extern HWND g_hWnd;
